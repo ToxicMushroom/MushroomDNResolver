dee244db23e2003376779dff934285a55de5b692
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2024-11-20 17:05:07 +0100

    order fix

--- src/authority/mushroom.rs
+++ src/authority/mushroom.rs
@@ -1,10 +1,9 @@
-use hickory_proto::op::Header;
-use hickory_proto::rr::RecordType;
-use hickory_resolver::lookup::Lookup;
-use hickory_resolver::{ResolveError, TokioResolver};
-use crate::authority::{MessageResponse, MessageResponseBuilder};
+use crate::authority::MessageResponseBuilder;
 use crate::lookup::hickory_lookup;
 use crate::server::{Request, RequestHandler, ResponseHandler, ResponseInfo};
+use hickory_proto::op::Header;
+use hickory_proto::rr::RecordType;
+use hickory_resolver::TokioResolver;
 
 pub struct Mushroom {
     pub resolver: TokioResolver
@@ -22,8 +21,8 @@ impl RequestHandler for Mushroom {
                 response_handle.send_response(mb.build(
                     Header::response_from_request(request.header()),
                     result.record_iter().filter(|x1| x1.record_type() != RecordType::SOA && x1.record_type() != RecordType::NS),
-                    result.record_iter().filter(|x1| x1.record_type() == RecordType::SOA),
                     result.record_iter().filter(|x1| x1.record_type() == RecordType::NS),
+                    result.record_iter().filter(|x1| x1.record_type() == RecordType::SOA),
                     vec![].into_iter())
                 ).await.expect("TODO: panic message")
             }
